39413bb5723184495c1909e52661844019b7dabf
koydirex <80644456+koydirex@users.noreply.github.com>
2021-07-21 17:44:30 +0300

    link in axios was change on github link

--- src/JS/index.js
+++ src/JS/index.js
@@ -2,7 +2,7 @@ let first_load=0
 function load_index(new_state=true){
     let info=null;
     console.log("индекс")
-    axios.get('http://localhost:63342/Saite/index.html')
+    axios.get('https://tttdd.github.io/Sites/src/index.html')
         .then((response)=> {
             // handle success
             let link=document.querySelector("#index")
