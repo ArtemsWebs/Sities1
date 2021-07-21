b6fdc5a11586d99ce90297841bfc27914dd561d7
koydirex <80644456+koydirex@users.noreply.github.com>
2021-07-21 22:22:21 +0300

    added check url

---
+++ src/JS/urlCheck.js
@@ -0,0 +1,11 @@
+let href=window.location.href
+function get_content() {
+    if (href.includes("page=location"))
+        load_location(false)
+    else if (href.includes("page=taimer")) {
+        load_time(false)
+    } else {
+        load_index(false)
+    }
+}
+get_content()
