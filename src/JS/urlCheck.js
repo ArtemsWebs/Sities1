5eddcea43ed39e83a019dd3fd5f0930493189cc0
koydirex <80644456+koydirex@users.noreply.github.com>
2021-07-23 19:06:01 +0300

    Добавлено ожидание загрузки страницы

--- src/JS/urlCheck.js
+++ src/JS/urlCheck.js
@@ -1,11 +1,10 @@
 let href=window.location.href
 function get_content() {
     if (href.includes("page=location"))
-        load_location(false)
-    else if (href.includes("page=timer")) {
-        load_time(false)
-    } else {
-        load_index(false)
-    }
+        document.addEventListener('DOMContentLoaded', function(){load_location(false)})
+    else if (href.includes("page=timer"))
+        document.addEventListener('DOMContentLoaded', function(){load_time(false)})
+     else
+        document.addEventListener('DOMContentLoaded', function(){load_index(false)})
 }
 get_content()
