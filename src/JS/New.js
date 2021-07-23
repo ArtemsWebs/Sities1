06cbe5a79aeb7e9ff1cb843882b676e10a52555c
koydirex <80644456+koydirex@users.noreply.github.com>
2021-07-23 17:58:16 +0300

    изменена чать url

--- src/JS/New.js
+++ src/JS/New.js
@@ -1,16 +1,13 @@
 document.body.querySelector(".low-title-right-items-container-activity-container").addEventListener('click',(e)=>{
-    console.log("Я тут")
     load_index();
     e.preventDefault();
 })
 document.body.querySelector(".low-title-right-items-container-map-container").addEventListener('click',(e)=>{
-    console.log("Я тут")
     load_location();
     e.preventDefault();
 
 })
 document.body.querySelector(".low-title-right-items-container-time-container").addEventListener('click',(e)=>{
-    console.log("Я тут")
     load_time();
     e.preventDefault();
 })
--- src/JS/urlCheck.js
+++ src/JS/urlCheck.js
@@ -2,7 +2,7 @@ let href=window.location.href
 function get_content() {
     if (href.includes("page=location"))
         load_location(false)
-    else if (href.includes("page=time")) {
+    else if (href.includes("page=timer")) {
         load_time(false)
     } else {
         load_index(false)
